7c04a713962dd13702ba4675eb8ea67d9da823d8
vladisov <vlad079@ya.ru>
2018-01-28 19:56:34 +0300

    added input + state

--- pages/crowdfunds/new.js
+++ pages/crowdfunds/new.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import { Button, Form, Input } from "semantic-ui-react";
 
 class CrowdfundNew extends Component {
+  state = {
+    minimumContribution: ""
+  };
+
   render() {
     return (
       <Layout>
-        <div>new campaign</div>
+        <h1>Create a Campaign!</h1>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
