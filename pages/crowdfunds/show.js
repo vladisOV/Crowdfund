03318c6c8a2b46abafde47fb91355e252e3b0136
Vladislav.Ovcharenko <Vladislav.Ovcharenko@x5.ru>
2018-01-29 16:50:12 +0300

    added summary to contract

--- pages/crowdfunds/show.js
+++ pages/crowdfunds/show.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Layout from "../../Layout";
 
 class CrowdfundShow extends Component {
   render() {
-    return <h3>asd</h3>;
+    return (
+      <Layout>
+        <h3>asd</h3>
+      </Layout>
+    );
   }
 }
 
