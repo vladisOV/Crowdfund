fdd119eab63e0d72ecd760bf557ad4a4c1b05d8e
Vladislav.Ovcharenko <Vladislav.Ovcharenko@x5.ru>
2018-01-29 11:56:56 +0300

    add some routes

--- pages/crowdfunds/new.js
+++ pages/crowdfunds/new.js
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout";
 import { Button, Form, Input, Message } from "semantic-ui-react";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
+import { Router } from "../../routes";
 
 class CrowdfundNew extends Component {
   state = {
@@ -21,6 +22,7 @@ class CrowdfundNew extends Component {
         .send({
           from: accounts[0]
         });
+      Router.pushRoute("/");
     } catch (err) {
       this.setState({ errorMessage: err.message });
       console.log(err);
